899f543501a5211ac89cc2563beb623473e18a0c
akshatarajashekar <akshatarajashekar@protonmail.com>
2022-07-05 22:19:03 +0530

    removed footer content

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className={`${cssPrefix} flexbox`}>
       <div className={`${cssPrefix}__container flexbox`}>
-        <div className={`${cssPrefix}__description`}>Welcome to Food Blog</div>
+        <div className={`${cssPrefix}__description`}></div>
       </div>
     </div>
   );
